b79d2bef9901aedf14d442de8044d6e593e86240
KeirnanO <45085114+KeirnanO@users.noreply.github.com>
2019-10-08 01:17:47 -0400

    Add files via upload

---
+++ Gun.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public bool attack;
+
+    public Transform muzzle;
+
+	protected int maxAmmo;
+	protected int ammo;
+
+    [SerializeField]
+    protected Transform cam;
+
+    [SerializeField]
+    protected GameObject bullet;
+
+    private void Update()
+    {
+        //cam = Camera.main.transform;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    //Instantiate bullets based on gun type
+    public virtual void Shoot(){}
+    //Set ammo
+    public virtual void SetAmmo(){}
+
+	public int GetAmmo()
+	{
+		return ammo;
+	}
+
+	public int GetMaxAmmo()
+	{
+		return maxAmmo;
+	}
+
+	public void Reload()
+	{
+		ammo = maxAmmo;
+	}
+}
---
+++ Pistol.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pistol : Gun
+{
+
+    override public void Shoot()
+    {
+        ammo--;
+
+        FindObjectOfType<AudioManager>().Play("Gunshot");
+
+        RaycastHit hit;
+
+        float spreadX = Random.Range(-1f, 1f);
+        float spreadY = Random.Range(-1f, 1f);
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit))
+        {
+            Transform newBullet = Instantiate(bullet, transform.position, transform.rotation).transform;
+            newBullet.LookAt(hit.point);
+            newBullet.Rotate(new Vector3(spreadX, spreadY, 0));
+
+        }
+        else
+        {
+            Instantiate(bullet, transform.position, Camera.main.transform.rotation);
+        }
+
+
+
+    }
+}
